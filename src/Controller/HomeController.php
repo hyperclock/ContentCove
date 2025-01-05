ea982528281e46d64c8470dc4ef643f13ad90cc7
hyperclock <hyperclock@main-jig.hyperclock.home>
2025-01-05 23:32:12 +0100

    UI language started. Language bar created. Sidebar created.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+// src/AppBundle/Controller/PageController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/sidebar', name: 'app_sidebar')]
+    public function sidebarAction()
+    {
+        return $this->render('home/sidebar.html.twig');
+    }
+
+}
